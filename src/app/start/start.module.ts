8e64e21288c626c1f96133e5de00c75c3e37f8a0
leopoglia <92129999+leopoglia@users.noreply.github.com>
2022-08-02 21:49:51 -0300

    very changes

--- src/app/start/start.module.ts
+++ src/app/start/start.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InitialComponent } from './initial/initial.component';
 import { LoginComponent } from './login/login.component';
+import { RankingComponent } from './ranking/ranking.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [InitialComponent, LoginComponent]
+  declarations: [InitialComponent, LoginComponent, RankingComponent]
 })
 export class StartModule { }
