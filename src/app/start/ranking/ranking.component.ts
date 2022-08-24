539f39dd89ac778cf7c6d5d182d61eba267c8425
K43RU <92130250+K43RU@users.noreply.github.com>
2022-08-24 14:28:06 -0300

    ok

--- src/app/start/ranking/ranking.component.ts
+++ src/app/start/ranking/ranking.component.ts
@@ -12,36 +12,36 @@ export class RankingComponent implements OnInit {
   ngOnInit() {
     this.rankingatual = this.ranking1;
 
-    for (let i = 0; i < this.rankings.length; i++) {
-      if (this.rankings[i].fase == 1) {
-        this.ranking1.push(this.rankings[i]);
-      } else if (this.rankings[i].fase == 2) {
-        this.ranking2.push(this.rankings[i]);
-      } else if (this.rankings[i].fase == 3) {
-        this.ranking3.push(this.rankings[i]);
-      } else if (this.rankings[i].fase == 4) {
-        this.ranking4.push(this.rankings[i]);
-      } else if (this.rankings[i].fase == 5) {
-        this.ranking5.push(this.rankings[i]);
-      } else if (this.rankings[i].fase == 6) {
-        this.ranking6.push(this.rankings[i]);
+    for (let i = 0; i < this.ranking.length; i++) {
+      if (this.ranking[i].fase == 1) {
+        this.ranking1.push(this.ranking[i]);
+      } else if (this.ranking[i].fase == 2) {
+        this.ranking2.push(this.ranking[i]);
+      } else if (this.ranking[i].fase == 3) {
+        this.ranking3.push(this.ranking[i]);
+      } else if (this.ranking[i].fase == 4) {
+        this.ranking4.push(this.ranking[i]);
+      } else if (this.ranking[i].fase == 5) {
+        this.ranking5.push(this.ranking[i]);
+      } else if (this.ranking[i].fase == 6) {
+        this.ranking6.push(this.ranking[i]);
       }
     }
   }
 
   fase(fase) {
     if (fase == 1) {
-      this.rankingatual = this.ranking1;
+      this.rankingatual = this.ranking1.sort((a, b) => { return a.tempo - b.tempo});
     } else if (fase == 2) {
-      this.rankingatual = this.ranking2;
+      this.rankingatual = this.ranking2.sort((a, b) => { return a.tempo - b.tempo});
     } else if (fase == 3) {
-      this.rankingatual = this.ranking3;
+      this.rankingatual = this.ranking3.sort((a, b) => { return a.tempo - b.tempo});
     } else if (fase == 4) {
-      this.rankingatual = this.ranking4;
+      this.rankingatual = this.ranking4.sort((a, b) => { return a.tempo - b.tempo});
     } else if (fase == 5) {
-      this.rankingatual = this.ranking5;
+      this.rankingatual = this.ranking5.sort((a, b) => { return a.tempo - b.tempo});
     } else if (fase == 6) {
-      this.rankingatual = this.ranking6;
+      this.rankingatual = this.ranking6.sort((a, b) => { return a.tempo - b.tempo});
     }
   }
 
@@ -49,8 +49,7 @@ export class RankingComponent implements OnInit {
 
 
   ranking = JSON.parse(localStorage.getItem('lista'));
-  rankings = this.ranking;
-  rankingatual;
+  rankingatual = this.ranking.sort((a, b) => {return a.tempo - b.tempo});
   ranking1 = [];
   ranking2 = [];
   ranking3 = [];
